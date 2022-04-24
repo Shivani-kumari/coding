7a67300520d69b8a32a8c9b2b05a48cf4e4014f1
Shivani-kumari <shivanikumari5@yahoo.com>
2022-04-24 22:34:28 +0530

    make table in html

---
+++ webApps/Table/table.js
@@ -0,0 +1,23 @@
+tableData= [
+    {
+    name: "shivani",
+    email:'shivanikumari5@yahoo.com' ,
+    phone:'9304631092',
+    id :'1' ,
+    status:'active'
+},
+{
+    name: "shivani",
+    email:'shivanikumari5@yahoo.com' ,
+    phone:'9304631092',
+    id :'2' ,
+    status:'active'
+},
+{
+    name: "shivani",
+    email:'shivanikumari5@yahoo.com' ,
+    phone:'9304631092',
+    id :'3' ,
+    status:'active'
+}
+]
